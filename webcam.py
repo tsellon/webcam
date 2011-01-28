2d04a394d9cfc120740360313fa9df19f1d736d1
Tyler <tsellon@gmail.com>
2011-01-28 17:28:04 +0100

    Refactored to make it easier to experiment with different filter combinations.

--- webcam.py
+++ webcam.py
@@ -3,14 +3,29 @@ from time import sleep
 import Image
 import ImageFilter
 import ImageChops
+import ImageOps
 
 cam = Device()
 cam.setResolution(640,480)
 
-for ii in range(10):
-    im = cam.getImage()
+def main():
+    for ii in range(10):
+        im = cam.getImage()
+        im = ApplyFilterSet2(im)
+        im.save('%i.jpg' % ii)
+        sleep(1)
+
+def ApplyFilterSet1(im):
     im2 = im.filter(ImageFilter.FIND_EDGES)
     im2 = im2.filter(ImageFilter.BLUR)
-    im = ImageChops.darker(im,im2)
-    im.save('%i.jpg' % ii)
-    sleep(1)
+    im = ImageChops.multiply(im,im2)
+    return im
+
+def ApplyFilterSet2(im):
+    im2 = im.filter(ImageFilter.FIND_EDGES)
+    im = ImageOps.solarize(im)
+    im = ImageChops.difference(im,im2)
+    return im
+
+if __name__ == "__main__":
+    main()
